68f3731236a1350ae21321197e78e57e329889fc
Mehdi Hassan Akash <mehdihassan.akash@iqvia.com>
2024-02-24 00:38:27 +0600

    Add error validation

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -10,6 +10,7 @@ func (app *application) logError(r *http.Request, err error) {
 }
 
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	app.logError(r, fmt.Errorf("%v", message))
 	env := envelope{"error": message}
 
 	err := app.writeJson(w, status, env, nil)
@@ -33,4 +34,12 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
